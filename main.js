6238ac0f0342146334b8009b713aad4daad4a0ed
juan casco <tomeoamigoivan@gmail.com>
2020-08-19 16:22:43 -0300

    con pistas, falta poco

--- main.js
+++ main.js
@@ -1,57 +1,78 @@
 // Creación del tablero
 function iniciar() {
-    var Tablero=document.getElementById("tablero");
+var Tablero=document.getElementById("tablero");
+Tablero.classList.add("bajando")
+Tablero.style.visibility="visible";
+document.getElementById("pieza").classList.add("opacidad");
 document.getElementById("boton").style.display="none";
 document.getElementById("danubio").play();
-setTimeout(aparecer,3000);
-Tablero.classList.add("bajar")
-Tablero.style.visibility="visible";
+setTimeout(teclas,7000);
+setInterval(pistass,110000);
+}
+function teclas(){
+    document.onkeydown=chars;
+    //Ahora se va a escuchar cuando el usuario apreta una tecla  
 }
 
-function aparecer(){
-  
-    for (i = 0; i <= 10; i++) {
-    setTimeout("document.getElementById('pieza').style.opacity = " + (i / 10) + "", i * 300);
-    setTimeout(teclas,3000);
-    
-        }
+
+
+/// PISTAS
+
+const animaciones=["subir-1", "bajar-1", "bajar-2", "subir-2"];
+var alternador=0;
+
+function pistass(){
+    var pista1=document.getElementById("pista-1")
+    var pista2=document.getElementById("pista-2")
+    var clues=[pista1, pista2, pista1, pista2]
+
+    if (alternador==4){
+        alternador=0;
     }
+
+   clues[alternador].classList.add(animaciones[alternador]);
+    setTimeout(desaparecer,10000)
+}
+function desaparecer(){
+    var pista1=document.getElementById("pista-1")
+    var pista2=document.getElementById("pista-2")
+    var clues=[pista1, pista2, pista1, pista2]
+
+    clues[alternador].classList.remove(animaciones[alternador])
+     alternador=alternador+1
+ 
+}
+
+
+
+
     function teclas(){
         document.onkeydown=chars;
+        //Ahora se va a escuchar cuando el usuario apreta una tecla  
     }
 
-window.onload= function tablero(){
-    var Tablero=document.getElementById("tablero");
-    console.log(Tablero.getBoundingClientRect());
+                // JUEGO PRINCIPAL
 
 
 
-
-}
-
-
-
-// Los movimientos permiten la continuidad del desplazamiento y 
 // tienen como valor los márgenes iniciales del div
-//console.log(pieza);
 var movimientoHorizontal=252;
 var movimientoVertical=441;
 
+// Importante para que levante la posibilida de escuchar cuando el usuario toca una tecla
 function chars (evento){
     if (window.event)
     evento=window.
-    caracteres(evento.keyCode);
-   
-  
+    caracteres(evento.keyCode);    
 }
 
-console.log(chars);
+//Función principal, se aplica al teclear
 
 function caracteres (chars) {
 
     var fichas=document.querySelectorAll(".ficha")
-    console.log(fichas);
     var ficha= document.getElementById("pieza");
+// Array para ir cambiando las imágenes del elemento "Pieza"
 const piezas=[
 "url(imagenes/peon.png)",
 "url(imagenes/peon.png)", 
@@ -63,7 +84,7 @@ const piezas=[
 ];   
 
 
-// movilidad
+// movilidad con las teclas direccionales
     if(chars==39) {
         movimientoHorizontal+=9;
     movimiento(ficha);
@@ -121,7 +142,16 @@ if(
     ficha.setAttribute("rey", "true");
 }
 
-// Función de transformar pieza
+
+
+// Función de movimiento con las reclas direccionales
+function movimiento(piece){
+    if (piece.getAttribute("inmovil")!="true") {
+    piece.style.marginLeft = movimientoHorizontal+"px";
+    piece.style.marginTop = movimientoVertical+"px";
+    }
+}
+// Función de transformar pieza al cruzar los límites del tablero
 function trasnformar(piece){
     var random=Math.abs(parseInt(Math.random()*movimientoVertical)%6);
    piece.removeAttribute("rey");
@@ -129,14 +159,9 @@ function trasnformar(piece){
     piece.style.backgroundImage = (piezas[random]);  
    }
 }
-// Función de movimiento
-function movimiento(piece){
-    if (piece.getAttribute("inmovil")!="true") {
-    piece.style.marginLeft = movimientoHorizontal+"px";
-    piece.style.marginTop = movimientoVertical+"px";
-    }
-}
-// JUEGO GANADO
+
+
+// JUEGO GANADO, cuando se lleva el rey a la casilla de origen
 if(
     (movimientoVertical==441)&&(movimientoHorizontal==252)
     &&(pieza.getAttribute("rey")=="true")
@@ -146,28 +171,33 @@ if(
     pieza.setAttribute("inmovil", "true");
     document.getElementById("danubio").pause();
     document.getElementById("aplausos").play();
+    document.getElementById("pista-1").style.display="none";
+    document.getElementById("pista-2").style.display="none";
 
     mostrar(fichas);
   danzar()
 
 }
+
+// Muestra un array de elementos, en este caso particular, las piezas
 function mostrar(elementos){
 for(i=0; i<elementos.length; i++){
     elementos[i].style.visibility="visible";
 }
 }
+
+
+// Animaciones para el final
 function danzar(){
     fichas[0].classList.add("volar-1");
     fichas[1].classList.add("volar-2");
     fichas[2].classList.add("volar-3");
     fichas[3].classList.add("volar-4");
-   // fichas[4].classList.add("circulo");
-   // fichas[5].classList.add("circulo-2");
+
 
     
 }
-// Necesito una función que me de valores aleatorios de 1 a 4 sin repetirse
-// Permutaciones dentro de [0,1,2,3]
+
 }
 
 
